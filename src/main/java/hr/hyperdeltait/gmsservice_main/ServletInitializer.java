5bb8975d098ef71e76477c813f94c8f48a7e2c9f
AnteMiholic <69636246+AnteMiholic@users.noreply.github.com>
2022-10-29 14:55:19 +0200

    Initial commit, works on tomcat

--- src/main/java/hr/hyperdeltait/gmsservice_main/ServletInitializer.java
+++ src/main/java/hr/hyperdeltait/gmsservice_main/ServletInitializer.java
@@ -3,6 +3,7 @@ package hr.hyperdeltait.gmsservice_main;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+//Tomcat
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
